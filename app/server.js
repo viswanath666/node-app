39ddf5b144237e9a211168dc4f788377e4fd11ee
Nana Janashia <nanajanashia@Nanas-MBP.home>
2020-08-23 14:59:09 +0200

    Adjust logs

--- app/server.js
+++ app/server.js
@@ -1,19 +1,18 @@
-var express = require('express');
+const express = require('express');
 const pino = require('pino');
 
-var app = express();
+const app = express();
 
-// pino constructor takes config object as param
 const logger = pino({ level: 'info' });
 
-logger.info('hello world');
-logger.info('hello elastic');
-logger.info('This is some great logging');
-logger.info('Some new entries in my-index');
+logger.info('hello elastic world');
+logger.info('This is some great stuff');
+logger.info('Some more entries for our logging');
 logger.info('another line');
-logger.info('index management');
-logger.info('Update your Elasticsearch indices');
-logger.info('you should see all these in Kibana');
+logger.info('This never stops');
+logger.info('Logging logging all the way');
+logger.info('I think this is enough');
+logger.info('nope, one more!');
 
 app.listen(3000, function () {
     logger.info("app listening on port 3000!");
